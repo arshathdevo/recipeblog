0360cb010dca1400d54c5d7d4b368d6f466e0055
arshathdevo <arshath.devo@gmail.com>
2026-03-09 06:47:19 +0530

    passwordchanged

--- .history/routes/recipeRoutes_20260214103713.js
+++ .history/routes/recipeRoutes_20260214103713.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const Recipe = require("../models/Recipe");
 
-
 // POST - Add new recipe
 router.post("/", async (req, res) => {
   try {
